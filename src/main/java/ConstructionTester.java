a3a5ef5084a3aaae374f9404509bcdd120bd1be9
IdanGershonMalka <51385334+IdanGershonMalka@users.noreply.github.com>
2020-10-01 09:44:58 -0700

    Update ConstructionTester.java

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,24 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+       
+        Scanner in = new Scanner(System.in);
+        
+        double sum;
+        
+        System.out.println("Enter the sales tax rate: ");
+        double taxRate = in.nextDouble();
+        System.out.println("How many boards do you need? ");
+        int boards = in.nextInt();
+        System.out.println("How many windows do you need? ");
+        int windows = in.nextInt();
+        
+        Construction myRate = new Construction(8,11,taxRate);
+
+        sum = myRate.lumberCost(boards) + myRate.windowCost(windows);
+       
+        
+        System.out.println("Total: " + sum + "\nGrand Total: " +  myRate.grandTotal(sum));
+ 
     }
 }
